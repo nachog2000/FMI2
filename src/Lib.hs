286d8fd7bbfb52eda9fb1868bf4714eccb9da176
Ignacio Garcia <nachog.2000@hotmail.com>
2020-06-08 11:46:08 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,24 @@ module Lib where
 import Text.Show.Functions
 
 laVerdad = True
+
+
+-- Punto 1)
+
+data Pais = UnPais {
+    ingresoPerCapita :: Float,
+    poblacionSectorPublico :: Float,
+    poblacionSectorPrivado :: Float,
+    recursosNaturales :: [String],
+    deudaConFMI :: Float
+}deriving (Show)
+
+namibia :: Pais
+namibia = UnPais {
+    ingresoPerCapita = 4140,
+    poblacionSectorPublico = 400000,
+    poblacionSectorPrivado = 650000,
+    recursosNaturales = ["Mineria","Ecoturismo"],
+    deudaConFMI = 50
+}
+
